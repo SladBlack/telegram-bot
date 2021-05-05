16776684115d65c21cb4e7b8f5e9ab9b71c270a9
Takhtilov Alexander <65767837+SladBlack@users.noreply.github.com>
2021-05-05 12:48:01 +0400

    update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-login = ''      # Логин от инсты
-password = ''   # Пароль от инсты
+login = 'primetgbot'      # Логин от инсты
+password = 'Xnw68SQj'   # Пароль от инсты
 token = '1799411857:AAHPObInzD4UdkHozoVBG88J0j7PU4Luhtc'    # Токен от бота телеграм
